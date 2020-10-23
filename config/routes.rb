304084a3db66eb7a1ab0a66d95f478430ee2deb0
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-23 11:39:49 -0600

    Add routes, controller, and view for amusement park show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # mechanics
+  get '/mechanics', to: 'mechanics#index'
+  get '/amusementpark/:id', to: 'amusementpark#show'
 end

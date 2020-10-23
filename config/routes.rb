81c8b7f7ba6180624f81a21a82b0acc1660fee1c
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-23 11:52:49 -0600

    Add routes, controller, and view for mechanic show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # mechanics
   get '/mechanics', to: 'mechanics#index'
   get '/amusementpark/:id', to: 'amusementpark#show'
+  get '/mechanic/:id', to: 'mechanic#show'
 end
